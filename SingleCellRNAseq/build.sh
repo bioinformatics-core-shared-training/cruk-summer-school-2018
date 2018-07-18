ba2d5f886555b6b7ce38385e1cc395bb61e1a7d7
Mark Fernandes <mfernandes61@gmail.com>
2018-07-18 11:05:13 +0100

    Prep. for new RNAseq materials

--- SingleCellRNAseq/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
